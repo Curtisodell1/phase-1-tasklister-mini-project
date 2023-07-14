0e34f69eb91b88efc1fc9aad67d52deac4d27ae9
Curtis O'Dell <curtisodell1@gmail.com>
2023-07-13 21:30:33 -0400

    simple use of submit button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  //Submit Button
+  document.getElementById("create-task-form").addEventListener('submit', (e) => {
+    e.preventDefault()
+    addToDo(e.target.newTaskDescription.value)
+  })
+  //Function to append to ToDo List
+  function addToDo(ToDo){
+    const li = document.createElement("li")
+    li.innerHTML = ToDo
+    document.getElementById("tasks").append(li)
+  }
 });
+
+
